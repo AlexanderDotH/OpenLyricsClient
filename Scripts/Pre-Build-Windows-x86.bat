563537bc8d278159ad9a66b0a6e1c5cf233e6bba
AlexH <mitteodersoyt@gmail.com>
2022-11-03 18:16:28 +0100

    Added icudtl.dat to the scripts

--- Scripts/Pre-Build-Windows-x86.bat
+++ Scripts/Pre-Build-Windows-x86.bat
@@ -1,3 +1,4 @@
 @echo off
 xcopy /e /k /h /i "..\Binaries\CefBinaries\win-x86\" "..\OpenLyricsClient\bin\Release\net6.0\publish\windows-x86\CefBinaries\win-x86\"
 xcopy /e /k /h /i "..\Binaries\IpaDic\" "..\OpenLyricsClient\bin\Release\net6.0\publish\windows-x86\IpaDic\"
+copy "..\Binaries\CefBinaries\icudtl.dat" "..\OpenLyricsClient\bin\Release\net6.0\publish\windows-x86\icudtl.dat"
--- Scripts/Pre-Build-Windows.bat
+++ Scripts/Pre-Build-Windows.bat
@@ -1,3 +1,4 @@
 @echo off
 xcopy /e /k /h /i "..\Binaries\CefBinaries\win-x64\" "..\OpenLyricsClient\bin\Release\net6.0\publish\windows-x64\CefBinaries\win-x64\"
 xcopy /e /k /h /i "..\Binaries\IpaDic\" "..\OpenLyricsClient\bin\Release\net6.0\publish\windows-x64\IpaDic\"
+copy "..\Binaries\CefBinaries\icudtl.dat" "..\OpenLyricsClient\bin\Release\net6.0\publish\windows-x64\icudtl.dat"
