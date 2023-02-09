6aa3f6188aa2695fb1635ae4cf5a4dbbffb78f2b
AlexH <mitteodersoyt@gmail.com>
2023-02-09 20:00:54 +0100

    Adjustments

--- OpenLyricsClient/Backend/Utils/WindowUtils.cs
+++ OpenLyricsClient/Backend/Utils/WindowUtils.cs
@@ -41,13 +41,15 @@ namespace OpenLyricsClient.Backend.Utils
         
         public static double GetScalingFactor()
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return 1.0;
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                return 1.0;
-
-            return (220 - GetWindowsScalingFactor()) / 100;
+            switch (GetWindowsScalingFactor())
+            {
+                case 100: return 1.0;
+                case 125: return 0.9;
+                case 150: return 0.8;
+                case 175: return 0.7;
+                
+                default: return 1.0;
+            }
         }
     }
 }
