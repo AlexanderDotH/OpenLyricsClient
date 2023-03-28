c6d5156ad5f32ce49c8b96879bdd5cced4d3572b
AlexanderDotH <einfacheinalex@gmail.com>
2023-03-28 14:03:19 +0200

    Null pointer

--- OpenLyricsClient/Frontend/View/Pages/LyricsPage.axaml.cs
+++ OpenLyricsClient/Frontend/View/Pages/LyricsPage.axaml.cs
@@ -65,7 +65,8 @@ public partial class LyricsPage : UserControl
 
         SolidColorBrush primaryBackColor = App.Current.FindResource("PrimaryBackgroundBrush") as SolidColorBrush;
         
-        if (Core.INSTANCE.SettingManager.Settings.DisplayPreferences.ArtworkBackground)
+        if (Core.INSTANCE!.SettingManager!.Settings!.DisplayPreferences != null && 
+            Core.INSTANCE!.SettingManager!.Settings!.DisplayPreferences!.ArtworkBackground!)
             primaryBackColor = App.Current.FindResource("SecondaryThemeColorBrush") as SolidColorBrush;
         
         Border border = new Border();
