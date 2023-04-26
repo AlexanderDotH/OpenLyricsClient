0404699b70cdd3c7be8e6aa620e8830fcdb4a4ff
AlexanderDotH <alexander.heuschkel@gmail.com>
2023-04-26 15:21:24 +0200

    Profile

---
+++ OpenLyricsClient/Frontend/Models/Pages/Settings/SettingsUserPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reactive;
+using System.Runtime.CompilerServices;
+using OpenLyricsClient.Backend;
+using ReactiveUI;
+
+namespace OpenLyricsClient.Frontend.Models.Pages.Settings;
+
+public class SettingsUserPageViewModel : ViewModelBase, INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
