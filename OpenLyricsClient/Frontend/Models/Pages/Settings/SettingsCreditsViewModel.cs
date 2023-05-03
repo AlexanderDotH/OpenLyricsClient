e0c0d057037442067e582d77b46d20289cc9d2bd
AlexanderDotH <alexander.heuschkel@gmail.com>
2023-05-03 17:28:01 +0200

    Added some ppl

--- OpenLyricsClient/Frontend/Models/Pages/Settings/SettingsCreditsViewModel.cs
+++ OpenLyricsClient/Frontend/Models/Pages/Settings/SettingsCreditsViewModel.cs
@@ -8,11 +8,12 @@ public class SettingsCreditsViewModel : ViewModelBase
 {
     public ReactiveCommand<Unit, Unit> OpenEimaenGithubCommand { get; }
     public ReactiveCommand<Unit, Unit> OpenD3SOXGithubCommand { get; }
-
+    public ReactiveCommand<Unit, Unit> OpenPKMNPlatinGithubCommand { get; }
 
     public SettingsCreditsViewModel()
     {
         OpenEimaenGithubCommand = ReactiveCommand.Create(()=>ProcessUtils.OpenBrowser("https://github.com/Eimaen"));
         OpenD3SOXGithubCommand = ReactiveCommand.Create(()=>ProcessUtils.OpenBrowser("https://github.com/D3SOX"));
+        OpenPKMNPlatinGithubCommand = ReactiveCommand.Create(()=>ProcessUtils.OpenBrowser("https://github.com/PKMNPlatin"));
     }
 }
